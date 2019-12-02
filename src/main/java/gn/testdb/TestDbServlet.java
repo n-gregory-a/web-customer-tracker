8858689020af4c5f35108b3103c91ee7a4662d5a
Grigory <ovonely@gmail.com>
2019-12-02 20:52:01 +0700

    some fixes

--- src/main/java/gn/testdb/TestDbServlet.java
+++ src/main/java/gn/testdb/TestDbServlet.java
@@ -5,11 +5,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Collections;
 
 @WebServlet("/TestDbServlet")
 public class TestDbServlet extends HttpServlet {
@@ -17,14 +15,14 @@ public class TestDbServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException {
 
         // setup connection variables
         String user = "springstudent";
         String pass = "springstudent";
 
         String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
-        String driver = "com.mysql.jdbc.Driver";
+        String driver = "com.mysql.cj.jdbc.Driver";
 
         // get connection to database
         try {
