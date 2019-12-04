b186dcd99f54e2784a7ca1a37f960ebedd26c961
Grigory <ovonely@gmail.com>
2019-12-04 21:53:44 +0700

    - modified CustomerController class to get the information from DAO and put it into the model - modified list-customers.jsp page to display customers table

--- src/main/java/gn/springDemo/controller/CustomerController.java
+++ src/main/java/gn/springDemo/controller/CustomerController.java
@@ -1,16 +1,30 @@
 package gn.springDemo.controller;
 
+import gn.springDemo.dao.CustomerDAO;
+import gn.springDemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // need to inject the customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String ListCustomers(Model model) {
 
+        // get customers from dao
+        List<Customer> customers = customerDAO.getCustomers();
+
+        // add the customers to the model
+        model.addAttribute("customers", customers);
 
         return "list-customers";
     }
